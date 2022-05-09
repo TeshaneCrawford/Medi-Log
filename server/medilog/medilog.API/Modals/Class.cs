f829fcb1f8384d6e14b5805999b12694da88d7c0
Teshane Crawford <crawfordteshane@gmail.com>
2022-05-09 12:16:28 -0500

    new field

--- server/medilog/medilog.API/Modals/Class.cs
+++ server/medilog/medilog.API/Modals/Class.cs
@@ -2,5 +2,11 @@
 {
     public class Class
     {
+        [Key]
+        public Guid id { get; set; }
+
+        public string name { get; set; }
+
+        public string role { get; set; }
     }
 }
